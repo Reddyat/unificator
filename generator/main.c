8a20bd7d7bea77a9e3ac7272f03fd13ab0226367
reddyat <reddyat@localhost.localdomain>
2018-10-14 19:27:01 +0200

    Handle SIGINT and make generator slower for the unificator sake.

--- generator/main.c
+++ generator/main.c
@@ -1,56 +1,82 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <time.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <signal.h>
 
+#define FILEPATH_SIZE_MAX 4096
+
+static int keep_running = 1;
+
+void int_handler()
+{
+    printf("Ctrl-C catched, stopping generator.\n");
+    keep_running = 0;
+}
 
 int main()
 {
-   	srand(time(NULL));
+    struct sigaction act = {0};
+    act.sa_handler = int_handler;
+    sigaction(SIGINT, &act, NULL);
 
-	//TODO : Take the directory where to create the file as argument
-	const char * output_directory = "/home/reddyat/workspace/unificator/working/";
+    srand(time(NULL));
+
+    //TODO : Take the directory where to create the file as argument
+    const char * output_directory = "/home/reddyat/workspace/unificator/working";
+    uint32_t filename = 0;
+
+    /* TODO : For the moment, we admit that if it fails, the directory already exists.
+     * We do not consider a wrong path or bad rights. We will see this later. */
+    mkdir(output_directory, 0777);
+
+    while ( keep_running )
+    {
+        /* Generate the full filename to create */
+        char hidden_filepath[FILEPATH_SIZE_MAX];
+        char visible_filepath[FILEPATH_SIZE_MAX];
+        snprintf(hidden_filepath, FILEPATH_SIZE_MAX, "%s/.%"PRIu32, output_directory, filename);
+        snprintf(visible_filepath, FILEPATH_SIZE_MAX, "%s/%"PRIu32, output_directory, filename);
+
+        /* Open the file and fill it with integers !!! */
+        FILE * file_pointer = NULL;
+        file_pointer = fopen(hidden_filepath, "w");
+
+        if(file_pointer == NULL)
+        {
+            fprintf(stderr, "Error : unable to create file %s\n", hidden_filepath);   
+            return -1;
+        }
 
 
-	/* TODO : For the moment, we admit that if it fails, the directory already exists.
-	 * We do not consider a wrong path or bad rights. We will see this later. */
-	mkdir(output_directory, 0777);
+        for ( int j = 0; j < 5000; j++)
+        {
+            /* Generate our unsigned integer for filling the file */
+            /* TODO : Create a random generator function to reach max of unsigned int */
+            uint32_t random = rand();
+            fprintf(file_pointer, "%u\n", random);
+        }
 
-	for (int i = 1; i < 200; i++)
-	{
-		/* Convert integer i in string */
-		char filename[10];
-		sprintf(filename, "%d", i);
+        fclose(file_pointer);
 
-		/* Generate the full filename to create */
-		char full_filename[256];
-		strcpy(full_filename, output_directory);
-		strcat(full_filename, filename);
+        /* Let's make the file visible. */
+        rename(hidden_filepath, visible_filepath);
 
-		/* Open the file and fill it with integers !!! */
-		FILE * file_pointer = NULL;
-		file_pointer = fopen(full_filename, "w");
+        /* New filename. */
+        filename++;
 
-		if(file_pointer == NULL)
-   		{
-      		printf("Error : unable to create file %s\n", full_filename);   
-      		return -1;
-   		}
+        /* You are too fast for unificator, sorry dude. */
+        if ( filename % 1000 == 0 )
+        {
+            sleep(1);
+        }
+    }
 
-
-   		for ( int j = 0; j < 5000; j++)
-   		{
-	   		/* Generate our unsigned integer for filling the file */
-	   		/* TODO : Create a random generator function to reach max of unsigned int */
-			uint32_t random = rand();
-			fprintf(file_pointer, "%u\n", random);
-   		}
-
-   		fclose(file_pointer);
-	}
-
-	return 0;
+    return 0;
 }
